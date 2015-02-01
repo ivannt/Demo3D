8a3fc9f9102fe5f386894035d29043e97a736051
ivanntakov <ivanntakov@europe.com>
2015-02-01 19:39:37 +0000

    Some Fun

---
+++ DemoCube3D/scripts/slow.js
@@ -0,0 +1,9 @@
+﻿
+$(function(){
+    $('.show-experiment').on('click', function(evt) {
+        evt.preventDefault();
+        $('.fallback').fadeOut('slow', function() {
+            $('.test').fadeIn('slow');
+        });
+    });
+});
